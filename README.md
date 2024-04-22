8f9b12349708f01e66a3b0bb4a9b8476cfee4d71
William Vallet <valletw@gmail.com>
2024-04-22 15:53:48 +0200

    Create project base: platformio and chip information on console

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 # ESP32 UPnP remote controller
 
-UPnP controller with IR/Bluetooth remote using ESP32 chip (check
-[esp-ir-receiver](https://github.com/valletw/esp-ir-receiver)).
+The purpose of this project is to create an UPnP control point controlled by
+infrared or bluetooth remote. It works with custom
+[esp-ir-receiver](https://github.com/valletw/esp-ir-receiver) based on ESP32 chip.
+
+## Software design
+
+```mermaid
+flowchart
+    IR_R([IR Remote])
+    BT_R([BT Remote])
+    IR_D(IR Decoder)
+    BT_S(BT/BLE Stack)
+    Commands(Commands Processing)
+    UPnP_CP(UPnP Control Point)
+    UPnP_R([UPnP Renderer])
+
+    IR_R -. "Infra-Red" .-> IR_D
+    BT_R -. Bluetooth .-> BT_S
+    IR_D --> Commands
+    BT_S --> Commands
+    Commands --> UPnP_CP
+    UPnP_CP -. WiFi .-> UPnP_R
+```
+
+## Build and program
+
+For fast development, this project uses PlatformIO tools.
+
+```shell
+# Build project.
+pio run
+
+# Clean build files.
+pio run --target clean
+
+# Upload firmware.
+pio run --target upload
+
+# Erase device.
+pio run --target erase
+```
---
+++ src/main.c
@@ -0,0 +1,45 @@
+/*
+ * MIT License
+ * Copyright (c) 2024 William Vallet
+ */
+
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include <stdio.h>
+#include <stdint.h>
+
+void app_main(void)
+{
+    printf("*** ESP UPnP remote ***\r\n");
+    // Get chip information.
+    esp_chip_info_t chip_info;
+    uint32_t flash_size = 0;
+    esp_chip_info(&chip_info);
+    esp_flash_get_size(NULL, &flash_size);
+    printf(
+        "Chip: %s [%d] (%d core(s)) rev %d.%d\r\n",
+        CONFIG_IDF_TARGET, chip_info.model, chip_info.cores,
+        chip_info.revision / 100, chip_info.revision % 100);
+    printf(
+        "Flash: %lu MB (%s)\r\n",
+        flash_size / (1024 * 1024),
+        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    printf(
+        "PSRAM: %s\r\n",
+        (chip_info.features & CHIP_FEATURE_EMB_PSRAM) ? "Embedded" : "None");
+    printf(
+        "Features:%s%s%s%s\r\n",
+        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? " WiFi" : "",
+        (chip_info.features & CHIP_FEATURE_BT) ? " BT" : "",
+        (chip_info.features & CHIP_FEATURE_BLE) ? " BLE" : "",
+        (chip_info.features & CHIP_FEATURE_IEEE802154) ? " IEEE-802.15.4" : "");
+    // Process.
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        printf(".");
+    }
+}
